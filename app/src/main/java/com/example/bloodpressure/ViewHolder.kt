0068405ad8db367e9c658fb75a1ad18a82f5d73b
shintaro417 <shintaro11417@gmail.com>
2021-03-24 11:27:12 +0900

    RecyclerView.ViewHolderを継承するクラスの作成(seventh)

---
+++ app/src/main/java/com/example/bloodpressure/ViewHolder.kt
@@ -0,0 +1,23 @@
+package com.example.bloodpressure
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.one_result.view.*
+
+/**
+ * RecyclerView.ViewHolderを継承するクラス
+ */
+class ViewHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
+    var dateText:TextView? = null
+    var minMaxText:TextView? = null
+    var pulseText:TextView? = null
+    
+    //初期化処理の実施
+    init{
+        //ビューホルダーのプロパティとレイアウトのViewの対応
+        dateText = itemView.dateText
+        minMaxText = itemView.minMaxText
+        pulseText = itemView.pulseText
+    }
+}
